041f31bf05bc96ec6b16754d32136dd0469aae90
Joanne Hoar <joanne.hoar@gmail.com>
2026-05-13 13:10:49 -0600

    Product service added

---
+++ src/common/ProductService.js
@@ -0,0 +1,17 @@
+const allProducts = [
+  {id: 1001, name: 'Cat', price: 100, description: 'A calm companion with a soft coat and a curious personality.', imageUrl: '/products/cat.png'},
+  {id: 1002, name: 'Dog', price: 200, description: 'A loyal friend that brings energy, playfulness, and company to the home.', imageUrl: '/products/dog.png'},
+  {id: 1003, name: 'Rabbit', price: 150, description: 'A gentle pet with quiet charm, long ears, and a love for cozy spaces.', imageUrl: '/products/rabbit.png'},
+ ];
+
+ class ProductService{
+    getAllProducts(){
+        return allProducts;    
+    }
+
+    getProductById(id){
+        return allProducts.find(product => product.id === id);    
+    }
+ }
+
+ export default new ProductService();
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,27 +1,23 @@
 import './ProductPage.css'
 import ProductCard from '../components/ProductCard.jsx'
+import ProductService from '../common/ProductService.js';
 
 function ProductPage(){
+    const allProducts = ProductService.getAllProducts();
 
-    const allProducts = [
-  {id: 1001, name: 'Cat', price: 100, description: 'A calm companion with a soft coat and a curious personality.', imageUrl: '/products/cat.png'},
-  {id: 1002, name: 'Dog', price: 200, description: 'A loyal friend that brings energy, playfulness, and company to the home.', imageUrl: '/products/dog.png'},
-  {id: 1003, name: 'Rabbit', price: 150, description: 'A gentle pet with quiet charm, long ears, and a love for cozy spaces.', imageUrl: '/products/rabbit.png'},
- ];
-
- function displaySelected(product){
-    alert(product.name);
- }
-
-return(
-    <div className="product-grid">
-    {
-        allProducts.map(product => (
-            <ProductCard key={product.id} product={product} onAction={displaySelected}/>
-        ))
+    function displaySelected(product){
+        alert(product.name);
     }
-    </div>
-)
+
+    return(
+        <div className="product-grid">
+        {
+            allProducts.map(product => (
+                <ProductCard key={product.id} product={product} onAction={displaySelected}/>
+            ))
+        }
+        </div>
+    )
 }
 
 export default ProductPage;
